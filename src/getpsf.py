a05b157dd37e57218e510bdd0884e8e74ade6c39
Benjamin Proudfoot <benp175@gmail.com>
2020-04-08 12:49:05 -0600

    Completes getpsf.py

--- src/getpsf.py
+++ src/getpsf.py
@@ -11,7 +11,10 @@
 # It also includes some test functions
 # test_getpsf_2dgau
 
+import math
 import numpy as np
+import astropy.modeling.functional_models
+import matplotlib.pyplot as plt
 
 # getpsf_2dgau
 # Inputs: 
@@ -20,9 +23,18 @@ import numpy as np
 #  - covariance matrix that describes the shape of the 2d Guassian, default=((1,0),(0,1))
 # Output: a numpy 2d array with the given size that has a *normalized* 2d Gaussian
 
-def getpsf_2dgau(size = 21):
-	psf = np.ones(size,size)
-	return psf
+def getpsf_2dgau(size = (21,21), cov = np.array([[1,0],[0,1]])):
+	# Creating the gaussian model with center at the center of the image
+	gaussian = astropy.modeling.functional_models.Gaussian2D(amplitude = 1, 
+	x_mean = (size[0]-1)/2, y_mean = (size[1]-1)/2, cov_matrix = cov)
+	
+	# Evaluating the psf at grid values
+	psf = np.zeros(size)
+	for i in range(size[0]):
+		for j in range(size[1]):
+			psf[i,j]=gaussian.evaluate(i,j,gaussian.amplitude,gaussian.x_mean,
+			gaussian.y_mean,gaussian.x_stddev,gaussian.y_stddev,gaussian.theta)
+	return psf/psf.sum()
 
 # Notes: 
 # Watch out for how to do the centering when the size in one direction is odd vs. even
@@ -36,10 +48,28 @@ def getpsf_2dgau(size = 21):
 # Before returning, normalize the PSF by dividing the whole 2d array by the total of the whole array
 
 
-
-
 # test_getpsf_2dgau
 # Runs a test of getpsf_2dgau
 # inputs values big enough to make an image (300,300) of a rotaated 2d Guassian
 # checks that the sum of the returned image is 1
 # plots the image and saves to a file
+
+def test_getpsf_2dgau(size = (300,300), cov = np.array([[5,1],[1,5]])):
+	# Getting psf from getpsf_2dgau
+	psf = getpsf_2dgau(size = size, cov = cov)
+	
+	# Check if sum == 1 using math.isclose due to floating point errors
+	if not math.isclose(psf.sum(), 1.0, rel_tol = 1e-10):
+		print("Error in getpsf_2dgau: Sum of psf is not 1.0")
+		print("PSF sum = ",psf.sum())
+	# Plot the psf to see what it looks like
+	plt.figure()
+	plt.imshow(psf, cmap='hot', interpolation='nearest')
+	plt.show()
+	#plt.savefig()		Make this have a place to save
+	plt.close()
+
+
+test_getpsf_2dgau()
+test_getpsf_2dgau(size = (50,50), cov = np.array([[10,3],[3,3]]))
+
