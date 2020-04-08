be8654ba6b298e74dab647454d558b84a6f10e8d
Rochelle Biancardi <biancarr@localhost.localdomain>
2020-04-08 12:18:07 -0600

    continue work on npsf_run_new.py

--- src/npsf_run_new.py
+++ src/npsf_run_new.py
@@ -3,10 +3,24 @@
 # Rochelle Steele
 # April 8, 2020
 #
-# Input:
-# Output: Chains from run
+# This code takes information about the run, likelihood calculations and parameters and
+# runs emcee to get the posteriors in a chain.
+#
+# Input: run_prop (JSON dictionary), npsf_likelihood (Jarrod), parameters df/array (Ian)
+# Output: Chains of parameters from run
 
+""" Run nPSF
+Inputs:
+    JSON file with properties of run
+    log-likelihood calculations (Jarrod)
+    parameters as df/array (Ian)
+
+Outputs:
+    chain from the run with posteriors
+
+"""
 ### THIS CODE IS NOT FUNCTIONING YET ###
+
 
 import numpy as np
 import emcee
@@ -16,28 +30,37 @@ import pandas as pd
 # from Ian import df_to_array
 
 
-
-# Define varaibles?
+# Define varaibles
+# ndim - Ian ?
+# npsf_likelihood -- from Jarrod?
+# vvv in run_props vvv
+nburnin = 0 # number of steps of burnin (will be deleted)
+nsteps = 5000 # number of steps
 walkers = 50
- # ndim
- # npsf_likelihood -- from Jarrod?
-nburnin = 0
-nsteps = 500
+filename = "results.csv"
 
-# Get initial guesses
+# Get initial guesses - from who???
+# p0
+
+# parameters from Ian
 
 # Check for bad walkers
 
-# Decide number of steps
-
-# How parameters are there?
-
 # Create sampler object
-
 sampler = emcee.EnsembleSampler(walkers, ndim, npsf_likelihood, args = )
 
 # Burnin (optional, default 0 steps)
-
 state = sampler.run_mcmc(p0, nburnin)
+sampler.reset()
 
 # Sample
+sampler.run_mcmc(state, nburnin)
+
+# Get chain
+chain = sampler.get_chain()
+flatchain = sampler.get_chain(flat = True)
+
+# save chain (how?)
+# Convert chain to df (Ian)
+# Save df as csv file
+
