433b17544b7af09faa122231e0446091bfa76f18
Physics 529 <p529@localhost.localdomain>
2020-04-07 18:25:22 -0600

    Some skeleton code for the helper functions and several notes for them.

--- src/params.py
+++ src/params.py
@@ -41,8 +41,6 @@ def params_to_fitarray(param_df, fixation_vals):
     #    -Find out what kind of array emcee uses.
     
     # Notes:
-    #    -I'm guessing that someone else will be handling the fixation_vals,
-    #        so I don't need to worry about making them, just using them.
     #    -So it sounds like emcee cannot use a dataframe, so this function
     #        will take the necessary values out of the dataframe and put them
     #        into an array.
@@ -52,13 +50,39 @@ def params_to_fitarray(param_df, fixation_vals):
     #    -What kind of array does emcee use that this function will need to give it?
     #        (e.g., what values will we want?)
     #    -For clarity: The change dictionary will only be loaded with things that
-    #        the fixation values array will say are floating?  Is it a string array?
-    #        Or maybe it's a dictionary in and of itself?
+    #        the fixation values array will say are floating? So parameter names and
+    #        indexes?
     #    -What all will be in the parameters dataframe?
+    #    -Just what will the fixation values array look like?
     
     emcee_arr = param_df.as_matrix()
     
     # Dictionary assignment will look something like this:
-    # change_dict["param"] = array_val
+    # change_dict["param"] = array_val (probably an index)
     
     return emcee_arr, change_dict
+
+def fitarray_to_paramsdf(emcee_arr, change_dict):
+    """Converts the array that emcee uses and the dictionary that
+    tracks which parameter name was assigned to which value in the
+    array to a parameters dataframe and information about which
+    parameters are to be held fixed or floating.
+    
+    Parameters:
+    
+    emcee_arr - An array that emcee can use.
+    
+    change_dict - A dictionary that tracks which parameter name was
+        assigned to which value in the array.
+    
+    Returns:
+    
+    param_df - The parameters dataframe
+    
+    fixation_vals - Information about which parameters are to be held
+        fixed or floating.
+    """
+    param_df = pd.DataFrame(emcee_arr,
+                            colums = ["What the columns are"])
+    
+    return param_df, fixation_vals
