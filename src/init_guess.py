9a76d4a7cbd4ae92c2289d999d2aa25b9f0945b6
Physics 529 <p529@localhost.localdomain>
2020-04-10 23:14:31 -0600

    This is the function that will provide the initial guess based on mean and std-dev.  It is NOT working properly, currently.

--- src/init_guess.py
+++ src/init_guess.py
@@ -21,6 +21,7 @@
 # to see that this works.  
 
 import pandas as pd
+import numpy as np
 
 def init_guess(start_guess_df):
     """This function will produce the initial guess used in nPSF.
@@ -38,6 +39,45 @@ def init_guess(start_guess_df):
         as start_guess_df and nwalker rows drawn from the 
         distribution.
     """
+    
+    # The mean will be the center of our distribution.
+    # The standard deviation will be the width.
+    # With these ideas in mind, I can potentially figure out the 
+    #    Gaussian distribution, but there might be a pre-existing
+    #    function to do that.
+    # So there is: np.random.normal(mu, sigma, nwalkers)
+    
+    # With these tools, we can just make the items for each column.
+    # The only thing I'm wondering about is how to choose the number
+    #    of walkers.  I'll choose 100 for now, until we can pick a
+    #    better value.  As far as I know, they should all be the same
+    #    number for the whole dataframe.
+    
+    # A guess for the number of walkers and taking the data in 
+    #    array form.
+    nwalkers = 100
+    arrSet = start_guess_df.as_matrix()
+    
+    # Some code to help us get the names for the columns.
+    name_dict = {0:"Junk"}
+    n = 0
+    dist_arr = []
+    for col in start_guess_df.columns:
+        name_dict[n] = col
+        infos = test_df[col].as_matrix()
+        mean1, stdev1 = infos[0],infos[1]
+        if n == 0:
+            dist_arr = np.random.normal(mean1,stdev1,10)
+        else:
+            dist_arr = np.vstack((dist_arr,np.random.normal(mean1,stdev1,10)))
+        
+        n += 1
+    
+    dict_vals = []
+    for x in name_dict:
+        dict_vals.append(name_dict[x])
+    
+    params_df = pd.DataFrame(dist_arr, columns = [dict_vals])
     
     
     return params_df
