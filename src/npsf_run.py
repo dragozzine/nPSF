ede41296a3ade7f7197b9fbe49c0ea3c1ec15ee1
Rochelle Biancardi <biancarr@localhost.localdomain>
2020-04-14 15:34:50 -0600

    npsf_run.py, now gets run properties (run_props) from JSON file (run_props.json)

--- src/npsf_run.py
+++ src/npsf_run.py
@@ -1,4 +1,4 @@
-# npsf_run_new.py
+# npsf_run.py
 #
 # Rochelle Steele
 # April 8, 2020
@@ -33,25 +33,79 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import corner
+import json
 # from likelihood import log_probability # Jarrod's code for EnsembleSampler
 # from params.py import df_to_array, npsf_init_guess()
 np.random.seed(42)
 
-
+json_file = "run_props.json"
 ndim = 5 # will get from Ian's params.py code
 # npsf_likelihood -- from Jarrod?
-# vvv in run_props vvv
-nburnin = 0 # number of steps of burnin (will be deleted)
-nsteps = 5000 # number of steps
-nwalkers = 32 # number of walkers
-filename = "results.csv" # name of output file containing  flatchain
 
+def create_run_props():
+    """ Creates a dictionary, run_props, with the properties of the run. Probably won't
+    end up using it, instead use load_run_props() which load them from a JSON file
+    
+    Inputs: none
+    
+    Outputs: run_props (dictionary of properties of the run)
+    """
+    # Create dictionary
+    run_props = {"nwalkers":"32","nburnin":"0","nsteps":"5000","output_filename":"results.csv"}
+    
+    return run_props
+
+def save_run_props(run_props, json_file):
+    """ Saves an already defined run_props dictionary to a JSON file. May not end up being used
+    that much or just for changing the JSON file.
+    
+    Inputs: run_props (dictionary), json_file (filename of JSON file)
+    
+    Outputs: Saves a JSON file. Returns nothing.
+    """
+    # Convert to json and save
+    with open(json_file,'w') as file:
+        json.dump(run_props, file)
+    
+    return
+
+def load_run_props(json_file):
+    """ Imports run_props (dictionary) from a JSON  file
+    
+    Input: json_file -- name of JSON file used
+    
+    Output: run_props -- dictionary of properties of the run
+    """
+    with open(json_file,'r') as file:
+        run_props = json.load(file)
+    return run_props
+
+def read_run_props(run_props):
+    """ Reads the dictionary run_props and saves each entry as a separate variable
+    
+    Input: run_props -- dictionary with properties of run
+    
+    Outputs:
+      - nwalkers -- number of walkers used for the run
+      - nburnin -- number of steps in burning, will be deleted (can be 0)
+      - nsteps -- number of steps used for run
+      - output_filename - name of the file that outputs the results (flatchain)
+    """
+    nwalkers = run_props['nwalkers']
+    nburnin = run_props['nburnin']
+    nsteps = run_props['nsteps']
+    output_filename = run_props['output_filename']
+
+    return nwalkers, nburnin, nsteps, output_filename
 
 def log_probability(x, mu, cov):
     """ Test code for the log probaiblity before Jarrod's code (likelihood) is ready
     This put gives the log probability of a Gaussian to be used in EnsembleSampler
+    
     Input: x (p0), mu (mean), cov (Sigma)
+    
     Output: log probability of Gaussian
+    
     From: https://emcee.readthedocs.io/en/stable/tutorials/quickstart/
     """
     diff = x - mu
@@ -61,8 +115,11 @@ def means_cov():
     """ More test code. This is needed for log_probability (test version).
     This code calculated the mean and cov of a Gaussian that will be created
     in log_probability.
+    
     Input: [none]
+    
     Output: means, cov 
+
     From: https://emcee.readthedocs.io/en/stable/tutorials/quickstart/
     """
     means = np.random.rand(ndim)
@@ -78,7 +135,9 @@ def burnin(p0, nburnin):
     """ This code determines if running the burnin is necessary (based on
     run_props) and then either runs a burnin and returns the resultant state
     or simply returns the initial guess, p0
+
     Inputs: p0 (initial guess), nburnin (number of steps to run the burnin)
+
     Output: the state where the sampler will start, either p0 or the state
       after the burnin, depending on whether or not the burnin was run
     """
@@ -92,9 +151,12 @@ def burnin(p0, nburnin):
 def make_hist(flatchain, param):
     """ Makes histogram of for one parameter. I will probably not call this
     anymore now that I can make corner plots, but it's still accessible.
+
     Input: flatchain (2D version of results from sampler), param (the index
       of the parameter to be used.
+
     Output: Saves an image of the histogram. Returns nothing.
+
     From: https://emcee.readthedocs.io/en/stable/tutorials/quickstart/
     """
     plt.hist(flatchain[:, param], 100, color="k", histtype="step")
@@ -107,12 +169,18 @@ def make_hist(flatchain, param):
 def make_corner_plot(flatchain):
     """ Makes a corner plot from the data from sampler. I may continue to
      make adjustments to this function.
+
     Input: flatchain (2D version of data from sampler)
+
     Output: Saves an image of the corner plot. Returns nothing.
     """
     fig = corner.corner(flatchain)#, labels=labels, truths=[m_true, b_true, np.log(f_true)])
     fig.savefig('../results/emcee_corner_Gaussian.png')
     return
+
+### Load run_props from JSON file ###
+run_props = load_run_props(json_file)
+nwalkers, nburnin, nsteps, output_filename = read_run_props(run_props)
 
 #######################################
 ### For use when params.py is ready ###
